494feed7afa7eae912325c76c0e65758c0b41a37
Ilyich Vismara <ilyich@arkemis.it>
2023-06-15 12:23:38 +0200

    feat: add renderHeader function to columns

--- src/__mocks__/mockColumns.ts
+++ src/__mocks__/mockColumns.ts
@@ -26,7 +26,12 @@ const mockColumns = [
     render: (data: Record<string, unknown>) => `Mr. ${data.last_name}`,
   },
   { label: "Email", id: "email" },
-  { label: "Gender", id: "gender", hidden: true },
+  {
+    label: "Gender",
+    id: "gender",
+    hidden: true,
+    renderHeader: () => "Gender (M/F)",
+  },
   { label: "IP Address", id: "ip_address", sortable: false },
 ];
 
